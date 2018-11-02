5e597da28f12b6c311cdfc4fa020219b742e4ac4
Antonio Britto <antonioabritto@gmail.com>
2018-11-02 19:04:03 -0300

    add credo as a dependency

--- mix.exs
+++ mix.exs
@@ -19,7 +19,8 @@ defmodule Hangman.MixProject do
 
   defp deps do
     [
-      {:dictionary, path: "../dictionary"}
+      {:dictionary, path: "../dictionary"},
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
