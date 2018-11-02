4d5de36e1713ab5ed0ab9f44eef937905b6fc15b
Antonio Britto <antonioabritto@gmail.com>
2018-11-02 19:24:03 -0300

    add some docs and docspec

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,4 +1,7 @@
 defmodule Hangman do
+  @moduledoc """
+  The public API for the Hangman Game.
+  """
   alias Hangman.Game
 
   defdelegate new_game(), to: Game
--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -1,10 +1,20 @@
 defmodule Hangman.Game do
+  @moduledoc """
+    The implementation of the Hangman game API
+  """
   defstruct(
     turns_left: 7,
     game_state: :initalizing,
     letters: []
   )
 
+  @doc """
+    Returns a struct with the initial game state
+
+    ## Examples:
+      iex> Hangman.Game.new_game()
+      %Hangman.Game{}
+  """
   def new_game do
     %Hangman.Game{
       letters: Dictionary.random_word() |> String.codepoints()
